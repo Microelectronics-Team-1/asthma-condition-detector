32fb91dd1b1d0448362acdbeca89722e28577512
stanleymw <107821509+stanleymw@users.noreply.github.com>
2024-08-03 21:49:59 -0700

    Implement baseline control using rotary encoder

--- src/main.ino
+++ src/main.ino
@@ -1,8 +1,10 @@
-/*************************************************** 
- ****************************************************/
+// Asthma Condition Detector
+// stanleymw
+
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1351.h>
 #include <Adafruit_BME280.h>
+#include <EEPROM.h>
 #include <SPI.h>
 
 // Screen dimensions
@@ -10,7 +12,6 @@
 #define SCREEN_HEIGHT 128
 
 // PIN DEFINITIONS
-
 // oled
 #define SCLK_PIN 13
 #define MOSI_PIN 11
@@ -28,6 +29,11 @@
 #define MQ_135 A0
 #define TTL A1
 
+// Rotary Encoder
+#define RE_CLK 2
+#define RE_DT 3
+#define RE_SW 4
+
 // Color definitions
 #define BLACK           0x0000
 #define BLUE            0x001F
@@ -41,10 +47,10 @@
 // for bme280
 #define SEALEVELPRESSURE_HPA (1013.25)
 
-const float AQ_BASE = 57.0;
-const float TTL_BASE = 125.0;
-const float MQ_BASE = 330.0;
-const float HUMIDITY_BASE = 50.0;
+//const float AQ_BASE = 57.0;
+//const float TTL_BASE = 125.0;
+//const float MQ_BASE = 330.0;
+//const float HUMIDITY_BASE = 50.0;
 
 // Option 1: use any pins but a little slower
 // Adafruit_SSD1351 tft = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, CS_PIN, DC_PIN, MOSI_PIN, SCLK_PIN, RST_PIN);  
@@ -56,6 +62,7 @@ const float HUMIDITY_BASE = 50.0;
 Adafruit_SSD1351 tft = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, CS_PIN, DC_PIN, RST_PIN);
 Adafruit_BME280 bme; // I2C
 
+int prevCLK;
 void setup(void) {
   Serial.begin(9600); Serial.print("hello!");
 
@@ -64,6 +71,12 @@ void setup(void) {
   pinMode(MQ_135, INPUT);
   pinMode(TTL, INPUT);
 
+  pinMode(RE_CLK, INPUT);
+  pinMode(RE_DT, INPUT);
+  pinMode(RE_SW, INPUT_PULLUP);
+
+  prevCLK = digitalRead(RE_CLK);
+
   tft.begin();
 
   while (!bme.begin(0x76)) {
@@ -71,7 +84,7 @@ void setup(void) {
     delay(1000);
   }
 
-  tft.fillScreen(BLACK);
+  //tft.fillScreen(BLACK);
   //testdrawtext("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa, fringilla sed malesuada et, malesuada sit amet turpis. Sed porttitor neque ut ante pretium vitae malesuada nunc bibendum. Nullam aliquet ultrices massa eu hendrerit. Ut sed nisi lorem. In vestibulum purus a tortor imperdiet posuere. ", WHITE);
 
   Serial.println("init");
@@ -114,8 +127,8 @@ const uint8_t getRatioVal(float ratio) {
     };
 }
 
-const void displayRatio(String text, float ratio) {
-    tft.setTextColor(WHITE);
+const void displayRatio(String text, float ratio, bool selected = false) {
+    tft.setTextColor(selected ? RED : WHITE);
     tft.print(text);
 
     uint16_t col;
@@ -136,43 +149,89 @@ const void displayRatio(String text, float ratio) {
 
     tft.setTextColor(col);
     tft.println(ratio);
+    tft.setTextColor(WHITE);
   }
 
-void loop() {
-    collectDustData();
+float getBaseline(int idx) {
+  return (float)(EEPROM.read(idx) * 2);
+}
 
-    const float AQ_RATIO = analogRead(GROVE_AQ) / AQ_BASE;
-    const float TTL_RATIO = analogRead(TTL) / TTL_BASE;
-    const float MQ_RATIO = analogRead(MQ_135) / MQ_BASE;
-    const float HUM_RATIO = bme.readHumidity() / HUMIDITY_BASE;
+int currentSelection = 0;
+unsigned long lastPushed = 0;
+
+void updateOled() {
+    const float AQ_RATIO = analogRead(GROVE_AQ) / getBaseline(0);
+    const float TTL_RATIO = analogRead(TTL) / getBaseline(1);
+    const float MQ_RATIO = analogRead(MQ_135) / getBaseline(2);
+    const float HUM_RATIO = bme.readHumidity() / getBaseline(3);
 
     const float AVG_RATIO = (AQ_RATIO + TTL_RATIO + MQ_RATIO + HUM_RATIO)/4;
 
     tft.setCursor(0,0);
     tft.fillScreen(BLACK);
+    displayRatio("GrAQ: ", AQ_RATIO, currentSelection == 0);
 
-    displayRatio("Grove AQ: ", AQ_RATIO);
-    displayRatio("TTL: ", TTL_RATIO);
-    displayRatio("MQ135: ", MQ_RATIO);
-    displayRatio("Humidity: ", HUM_RATIO);
+    displayRatio("TTL: ", TTL_RATIO, currentSelection == 1);
+
+    displayRatio("MQ: ", MQ_RATIO, currentSelection == 2);
+
+    displayRatio("Hum: ", HUM_RATIO, currentSelection == 3);
 
     tft.setTextColor(WHITE);
-    tft.print("grove dust: ");
+    tft.print("Gr Dust: ");
     tft.println(concentration);
 
-    tft.print("Temperature = ");
-    tft.print(bme.readTemperature());
-    tft.println(" °C");
+    tft.print("Temp: ");
+    tft.println(bme.readTemperature());
 
-    //tft.print("Pressure = ");
-    //tft.print(bme.readPressure() / 100.0F);
-    //tft.println(" hPa");
-//
+    tft.print("Pressure: ");
+    tft.print(bme.readPressure() / 100.0F);
+    tft.println(" hPa");
+
     //tft.print("Approx. Altitude = ");
     //tft.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
     //tft.println(" m");
 
-    tft.println("------");
+    tft.println("--------");
 
     displayRatio("Overall: ", AVG_RATIO);
+}
+
+unsigned long lastOledUpdate = 0;
+void loop() {
+    if (digitalRead(RE_SW) == LOW) {
+      if (millis() - lastPushed > 50) {
+        currentSelection += 1;
+        if (currentSelection > 3) {
+          currentSelection = 0;
+        }
+      }
+
+      lastPushed = millis();
+    }
+
+    int currentCLK = digitalRead(RE_CLK);
+
+    if (currentCLK != prevCLK && currentCLK == 1){
+      byte n = EEPROM.read(currentSelection);
+      if (digitalRead(RE_DT) != currentCLK) {
+        // counter clockwise
+        EEPROM.write(currentSelection, n - 2);
+        //Serial.println("CCW");
+      } else {
+        EEPROM.write(currentSelection, n + 2);
+        //Serial.println("CW");
+      }
+      //Serial.println(n);
+    }
+
+    prevCLK = currentCLK;
+
+    if (millis() - lastOledUpdate > 1000) {
+        updateOled();
+        lastOledUpdate = millis();
+      }
+
+    //collectDustData();
+    delay(1);
 }
